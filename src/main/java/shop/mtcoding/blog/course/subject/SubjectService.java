efd96558d15bd3070a08683632b029c038d3a19c
getinthere <getinthere@naver.com>
2025-05-22 15:32:19 +0900

    교과목 버그 정리

--- src/main/java/shop/mtcoding/blog/course/subject/SubjectService.java
+++ src/main/java/shop/mtcoding/blog/course/subject/SubjectService.java
@@ -5,12 +5,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import shop.mtcoding.blog._core.errors.exception.Exception400;
 import shop.mtcoding.blog._core.errors.exception.Exception404;
 import shop.mtcoding.blog.course.Course;
 import shop.mtcoding.blog.course.CourseRepository;
 import shop.mtcoding.blog.course.exam.ExamResponse;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -43,6 +45,17 @@ public class SubjectService {
         Course coursePS = courseRepository.findById(courseId)
                 .orElseThrow(() -> new Exception404("과정을 찾을 수 없습니다"));
 
+        List<Subject> subjectListPS = subjectRepository.findByCourseId(coursePS.getId());
+
+        Boolean isSameNo = subjectListPS.stream().anyMatch(subject -> subject.getNo().equals(reqDTO.getNo()));
+
+
+        if (isSameNo) {
+            String subjectNos = subjectListPS.stream()
+                    .map(subject -> subject.getNo().toString())
+                    .collect(Collectors.joining(","));
+            throw new Exception400("동일한 교과목 번호는 등록할 수 없습니다. \\n현재 교과목 번호들 : [" + subjectNos + "]");
+        }
         subjectRepository.save(reqDTO.toEntity(coursePS));
     }
 }
